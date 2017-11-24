1619e10c60c5de8bd1e4ac936a0c5cb661176c4c
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2017-11-24 23:15:20 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(num)
   if num.modulo(3) == 0
-    if num mod 5 == 0
+    if num.modulo(5) == 0
       return "Fizzbuzz"
     end
     return "Fizz"
-  elsif num mod 5 == 0
+  elsif num.modulo(5) == 0
     return "Buzz"
   end
   return nil
